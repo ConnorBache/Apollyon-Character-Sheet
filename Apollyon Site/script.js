204a4ab446f271b99524a2e7b57967ac6e71a596
Connor Bache <50557502+ConnorBache@users.noreply.github.com>
2025-11-12 12:55:07 -0500

    Update Clay Army ability details
    Removed the phrase 'is immortal' from the details of the Clay Army ability.

--- Apollyon Site/script.js
+++ Apollyon Site/script.js
@@ -876,7 +876,7 @@
       {
         "mote": "Kative",
         "name": "Clay Army",
-        "details": "[Boost 4] Before any of your actions, you may place a Clay Golem adjacent which is immortal, cannot move and cannot be moved through. When an enemy takes an action next to one or more clay golems, they take Spirit damage.",
+        "details": "[Boost 4] Before any of your actions, you may place a Clay Golem adjacent which cannot move and cannot be moved through. When an enemy takes an action next to one or more clay golems, they take Spirit damage.",
         "desc": "4 BP before action: summon an immortal golem that does Spirit damage when adjacent enemies take actions."
       },
       {
